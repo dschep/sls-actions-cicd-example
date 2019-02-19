65cea3bae389c9e938257ed1b87a8714f6cf802c
Daniel Schep <dschep@gmail.com>
2019-02-19 17:32:06 -0500

    debugging

--- github-action-stage-helper.js
+++ github-action-stage-helper.js
@@ -2,3 +2,5 @@ const fs = require('fs')
 
 module.exports = () => process.env.GITHUB_EVENT_PATH && JSON.parse(
     fs.readFileSync(process.env.GITHUB_EVENT_PATH)).ref.split('refs/heads/')[1]
+console.log(process.env.GITHUB_EVENT_NAME)
+console.log(require('fs').readFileSync(process.env.GITHUB_EVENT_PATH).toString())
