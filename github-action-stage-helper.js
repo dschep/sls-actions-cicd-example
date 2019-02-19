924381ac511352cabf9b62384341c4efcefa3156
Daniel Schep <dschep@gmail.com>
2019-02-19 18:14:10 -0500

    ugh.. deleted refs dont have `refs/heads/` in them

--- github-action-stage-helper.js
+++ github-action-stage-helper.js
@@ -1,6 +1,11 @@
 const fs = require('fs')
 
-module.exports = () => process.env.GITHUB_EVENT_PATH && JSON.parse(
-    fs.readFileSync(process.env.GITHUB_EVENT_PATH)).ref.split('refs/heads/')[1]
-console.log(process.env.GITHUB_EVENT_NAME)
-console.log(require('fs').readFileSync(process.env.GITHUB_EVENT_PATH).toString())
+module.exports = () => {
+  if (!process.env.GITHUB_EVENT_PATH) return
+  const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH))
+  if (event.ref.startsWith('refs/heads/')) {
+    return event.ref.slice('refs/heads/'.length)
+  } else {
+    return event.ref
+  }
+}
