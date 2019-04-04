b50d0452d41311f801b23ee04663e90876d5192e
Daniel Schep <dschep@gmail.com>
2019-04-04 14:47:44 -0700

    doh. wrong path for pr url

--- comment/entrypoint.js
+++ comment/entrypoint.js
@@ -6,7 +6,7 @@ const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH))
 
 const info = spawnSync('sls', ['info']).stdout.toString();
 
-fetch(event.payload.pull_request.comments_url, {
+fetch(event.pull_request.comments_url, {
   method: 'POST',
   body: JSON.stringify({body: `Created deployment for your PR. Here's the output of \`sls info\`:
 \`\`\`
