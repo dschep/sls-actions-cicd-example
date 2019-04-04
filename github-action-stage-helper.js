9d4972be228fd7b1580f4d673e2160c89a236ce4
Daniel Schep <dschep@gmail.com>
2019-04-04 14:36:44 -0700

    add pr support to stage helper script

--- github-action-stage-helper.js
+++ github-action-stage-helper.js
@@ -3,6 +3,9 @@ const fs = require('fs')
 module.exports = () => {
   if (!process.env.GITHUB_EVENT_PATH) return
   const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH))
+  if (event.pull_request) {
+    return event.pull_request.head.ref
+  }
   if (event.ref.startsWith('refs/heads/')) {
     return event.ref.slice('refs/heads/'.length)
   } else {
