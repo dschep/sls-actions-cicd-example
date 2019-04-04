0fa58f90ced0001a587a7188678687b08aead022
Daniel Schep <dschep@gmail.com>
2019-04-04 13:36:56 -0700

    add local action to comment on PR with sls info

---
+++ comment/entrypoint.js
@@ -0,0 +1,16 @@
+const { spawnSync } = require('child_process');
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH))
+
+const info = spawnSync('sls', ['info']).stdout.toString();
+
+fetch(event.payload.pull_request.comments_url, {
+  method: 'POST',
+  body: JSON.stringify({body: info}),
+  headers: {
+    Authorization: `token ${process.env.GITHUB_TOKEN}`,
+    'Content-Type': 'application/json'
+  }
+}).then(r => r.json()).then(console.log)
