12e31c3f0f3a5706f80cdd279b103cfaeb2a4102
Daniel Schep <dschep@gmail.com>
2019-04-04 13:56:00 -0700

    more verbose comment

--- comment/entrypoint.js
+++ comment/entrypoint.js
@@ -8,7 +8,11 @@ const info = spawnSync('sls', ['info']).stdout.toString();
 
 fetch(event.payload.pull_request.comments_url, {
   method: 'POST',
-  body: JSON.stringify({body: info}),
+  body: JSON.stringify({body: `Created deployment for your PR. Here's the output of \`sls info\`:
+\`\`\`
+${info}
+\`\`\`
+  `}),
   headers: {
     Authorization: `token ${process.env.GITHUB_TOKEN}`,
     'Content-Type': 'application/json'
